f423c194eac51c98919818a8d3f1dfff9e49a363
Raghs3 <tinku.minku2803@gmail.com>
2024-09-16 06:12:13 +0530

    Fixed game

--- fizz_buzz_challenge.py
+++ fizz_buzz_challenge.py
@@ -21,11 +21,10 @@ def fizz_buzz(number: int) -> str:
 input("Play Fizz Buzz. Press ENTER to start")
 print()
 
-# tim way using while loop
 next_number = 0
 while next_number < 99:
     next_number += 1
-    print(fizz_buzz(next_number))
+    print("My go:", fizz_buzz(next_number))
     next_number += 1
     correct_answer = fizz_buzz(next_number)
     players_answer = input("Your go: ")
@@ -36,29 +35,3 @@ while next_number < 99:
         break
 else:
     print("Well done, you reached {}".format(next_number))
-
-
-# my way using for loop
-# for i in range (1, 101, 2):
-    # print("I say {} for {}, now your turn."
-    #       .format(fizz_buzz(i), i))
-
-
-    # game part
-    # user_input = input(": ")
-    # if user_input.casefold() != fizz_buzz(i + 1):
-    #     print("Oh no, you got it wrong! That's the end.")
-    #     break
-
-    # print("{}: {} was correct"
-    #       .format(i+1, user_input))
-
-
-    # testing part
-    # for j in range(i, 101):
-    #     if fizz_buzz(j) != fizz_buzz(i+1):
-    #         print("Oh no, you got it wrong! Try again.")
-    #     else:
-    #         print("{}: {} was correct"
-    #               .format(j, fizz_buzz(j)))
-    #         break
